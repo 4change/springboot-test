73109b16428df5aa45c1a1f8dd694068fefc82f8
4change <e_mail4gxf@163.com>
2023-12-20 17:31:54 +0800

    mapstruct-链接补充

--- java/src/main/java/com/fly/test/mapstruct/Test.java
+++ java/src/main/java/com/fly/test/mapstruct/Test.java
@@ -2,6 +2,12 @@ package com.fly.test.mapstruct;
 
 public class Test {
 
+    /**
+     * <a href="https://blog.csdn.net/qq_40194399/article/details/110162124">MapStruct最详细的使用教程，别在用BeanUtils.copyProperties ()</a>
+     * <a href="https://blog.csdn.net/zhujiakuan/article/details/121530650#t10">mapstruct 使用与问题解决</a>
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Student student = new Student("诸葛亮", 22);
         Teacher teacher = new Teacher("司马懿", 35);
