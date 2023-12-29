bd80df3383aa2d5319e59354472db0129c1e3f6c
4change <e_mail4gxf@163.com>
2023-12-29 14:06:53 +0800

    QuestionChooser——整合knife4j

--- question-chooser/src/main/java/com/fly/test/controller/SubjectController.java
+++ question-chooser/src/main/java/com/fly/test/controller/SubjectController.java
@@ -2,6 +2,8 @@ package com.fly.test.controller;
 
 import com.fly.test.entity.Subject;
 import com.fly.test.service.SubjectService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/subject")
+@Api(value = "Subject", tags = "主题")
 public class SubjectController {
 
     @Autowired
     private SubjectService subjectService;
 
     @GetMapping("/list")
+    @ApiOperation("列表查询")
     public List<Subject> list() {
         return subjectService.list();
     }
